0635ad39e491d9d781e9b1be66bc165b7cf4ec71
서채연 <chaney11@naver.com>
2024-11-14 10:13:45 +0900

    FEAT: StringToEnumConverterFactory 구현 enum을 등록하는 방식이 아닌 factory에 등록해 전역적으로 설정합니다.

--- src/main/java/com/enjoytrip/common/config/ConverterConfig.java
+++ src/main/java/com/enjoytrip/common/config/ConverterConfig.java
@@ -1,7 +1,6 @@
 package com.enjoytrip.common.config;
 
-import com.enjoytrip.common.util.StringToEnumConverter;
-import com.enjoytrip.social.domain.SocialType;
+import com.enjoytrip.common.util.StringToEnumConverterFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -11,9 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ConverterConfig implements WebMvcConfigurer {
 
+    private final StringToEnumConverterFactory stringToEnumConverterFactory;
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        //제네릭 컨버터 사용시, 명시적으로 타입 표시
-               registry.addConverter(String.class, SocialType.class, new StringToEnumConverter<>(SocialType.class));
+        registry.addConverterFactory(stringToEnumConverterFactory);
     }
 }
